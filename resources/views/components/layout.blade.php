37df9196b505190577ca578e567c2f7e5630ce74
Marcio Fão <marcio.lopes.fao@gmail.com>
2023-11-03 22:23:02 -0300

    usar função asset no layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
     <h1>{{ $title }}</h1>
