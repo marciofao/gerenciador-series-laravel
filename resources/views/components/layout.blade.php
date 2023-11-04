3644a290b10efdaac05d127118d0344b826bf600
Marcio Fão <marcio.lopes.fao@gmail.com>
2023-11-03 22:36:47 -0300

    finaluzar frontend com bootstrap

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,7 +7,10 @@
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
-    <h1>{{ $title }}</h1>
-    {{$slot}}
+    <div class="container">
+        <h1>{{ $title }}</h1>
+        {{$slot}}
+    </div>
+    
 </body>
 </html>
