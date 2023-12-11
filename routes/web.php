c70d9aba02ccd7dea69315257798e6e5060fa00a
marciofao <marcio.lopes.fao@gmail.com>
2023-12-11 20:22:03 -0300

    update index route

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
+
+Route::get('/', [SeriesController::class, 'index']);
 
 Route::get('/series', [SeriesController::class, 'index']);
 
